1101d72cb9559bd055dbcf43e24bc4d258ac7c0a
Neotopio <s.leininger@live.fr>
2023-02-07 12:15:29 +0100

    Please enter the commit message for your changes. Lines starting  with '' will be ignored, and an empty message aborts the commit.
     On branch main
     Your branch is up to date with 'origin/main'.
    
     Changes to be committed:
    	new file:   header.php
    	new file:   images/logo.svg
    	new file:   images/rock-n-roll-gesture-outlined-hand-symbol_icon-icons.com_57977.ico
    	new file:   index.php
    	new file:   style.css
    
     Changes not staged for commit:
    	modified:   index.php
    	modified:   style.css

---
+++ index.php
@@ -0,0 +1,170 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.3/font/bootstrap-icons.css">
+    <link rel="stylesheet" href="style.css">
+    <title>Acceuil</title>
+</head>
+
+<body>
+    <header>
+        <div class="container-fluide">
+            <div class="row bg-black text-white justify-content-end">
+                <div class="col-lg-2"></div>
+                <div class="col-lg-2"></div>
+                <div class="col-lg-1"></div>
+                <div class="col-lg-2">
+                    <img src="images/logo.svg" class=" logo" alt="">
+                </div>
+                <div class="col-lg-3"></div>
+                <div class="col-lg-2 justify-content-end"><a href="" class="text-white"><i class="bi bi-person">Se connecter</i></a>
+                    <a href="" class="text-white"><i class="bi bi-heart"></i></a>
+                    <a href="" class="text-white"><i class="bi bi-cart"></i></a>
+                </div>
+            </div>
+        </div>
+
+    </header>
+    <nav class="navbar navbar-expand-lg bg-body-tertiary">
+        <div class="container-fluid">
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                    <li class="nav-item dropdown">
+                        <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                            Femme
+                        </a>
+                        <ul class="dropdown-menu">
+                            <li><a class="dropdown-item" href="#">Action</a></li>
+                            <li><a class="dropdown-item" href="#">Another action</a></li>
+                            <li>
+                                <hr class="dropdown-divider">
+                            </li>
+                            <li><a class="dropdown-item" href="#">Something else here</a></li>
+                        </ul>
+                    </li>
+                    <li class="nav-item dropdown">
+                        <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                            Homme
+                        </a>
+                        <ul class="dropdown-menu">
+                            <li><a class="dropdown-item" href="#">Action</a></li>
+                            <li><a class="dropdown-item" href="#">Another action</a></li>
+                            <li>
+                                <hr class="dropdown-divider">
+                            </li>
+                            <li><a class="dropdown-item" href="#">Something else here</a></li>
+                        </ul>
+                    </li>
+                    <li class="nav-item dropdown">
+                        <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                            Enfants
+                        </a>
+                        <ul class="dropdown-menu">
+                            <li><a class="dropdown-item" href="#">Action</a></li>
+                            <li><a class="dropdown-item" href="#">Another action</a></li>
+                            <li>
+                                <hr class="dropdown-divider">
+                            </li>
+                            <li><a class="dropdown-item" href="#">Something else here</a></li>
+                        </ul>
+                    </li>
+
+                </ul>
+                <form class="d-flex" role="search">
+                    <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
+                    <button class="btn btn-outline-success" type="submit">Search</button>
+                </form>
+            </div>
+        </div>
+    </nav>
+    <main>
+
+
+
+
+    </main>
+    <footer>
+        <div class="container">
+            <div class="row">
+                <div class="col-12 col-sm-4 ">
+                    <p class="">
+                        Service Client
+                    </p>
+                    <p class="">
+                        Notre équipe SAV est disponible de 10:00 à 18:30.
+                    </p>
+                    <div class="text-medium mb-2 mt-3 d-flex">
+                        <a href="" class="  w-auto" >
+                            <span class="w-100 justify-content-center d-flex">Nous contacter !</span>
+                        </a>
+                    </div>
+                    <div class="text-medium mb-2 mt-3 d-flex">
+                        <a href="" class="  w-auto" >
+                            <span class="w-100 justify-content-center d-flex">FAQ</span>
+                        </a>
+                    </div>
+                    <div class="text-medium mb-2 mt-3 d-flex">
+                        <a href="" class="  w-auto" >
+                            <span class="w-100 justify-content-center d-flex">Service Retour</span>
+                        </a>
+                    </div>
+                    <div class="text-medium mb-2 mt-3 d-flex">
+                        <a href="" class="  w-auto">
+                            <span class="w-100 justify-content-center d-flex">Informations sur la taille</span>
+                        </a>
+                    </div>
+                    <div class="text-medium mb-2 mt-3 d-flex">
+                        <a href="" class="  w-auto" >
+                            <span class="w-100 justify-content-center d-flex">Annuler mon Abonnement BSC</span>
+                        </a>
+                    </div>
+                </div>
+                <div class="col-12 col-sm-4 footer-content-spacing-big">
+                    <p class=" bold font-size-l">
+                        Offre pour toi
+                    </p>
+                    <div class="text-medium mb-2 mt-3 d-flex">
+                        <a href="" class="  w-auto" >
+                            <span class="w-100 justify-content-center d-flex">Concours</span>
+                        </a>
+                    </div>
+                    <div class="text-medium mb-2 mt-3 d-flex">
+                        <a href="" class="  w-auto" >
+                            <span class="w-100 justify-content-center d-flex">Bons d'achat EMP</span>
+                        </a>
+                    </div>
+                    <div class="text-medium mb-2 mt-3 d-flex">
+                        <a href="" class="  w-auto">
+                            <span class="w-100 justify-content-center d-flex">Réduction EMP pour les étudiants</span>
+                        </a>
+                    </div>
+                </div>
+                <div class="col-12 col-sm-4 footer-content-spacing-big">
+                    <p class="font-secondary bold font-size-l">
+                        À propos d'EMP
+                    </p>
+                    <div class="text-medium mb-2 mt-3 d-flex">
+                        <a href="" class="  w-auto" >
+                            <span class="w-100 justify-content-center d-flex">Réseau d'Affiliation</span>
+                        </a>
+                    </div>
+                    <div class="text-medium mb-2 mt-3 d-flex">
+                        <a href="" class="  w-auto" >
+                            <span class="w-100 justify-content-center d-flex">Durabilité</span>
+                        </a>
+                    </div>
+                </div>
+            </div>
+            
+        </div>
+
+
+    </footer>
+</body>
+
+</html>
