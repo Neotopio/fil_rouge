cc0c37cbe7b0c4da1b72c694c88061c8e0024a76
Neotopio <s.leininger@live.fr>
2023-02-21 16:21:46 +0100

    Please enter the commit message for your changes. Lines starting  with '' will be ignored, and an empty message aborts the commit.
     On branch main
     Your branch is up to date with 'origin/main'.
    
     Changes to be committed:
    	new file:   controllers/subCategories.php
    
     Changes not staged for commit:
    	modified:   admin.php
    	modified:   database.php
    	modified:   model/categories.php
    	modified:   model/deleteCategories.php
    	modified:   model/deleteSubCategories.php
    	modified:   model/subCategories.php
    	modified:   model/validate.php
    	modified:   template/adCategories.php
    	modified:   template/adSubCategories.php
    	modified:   template/categories.php
    	modified:   template/subCategories.php
    
     Untracked files:
    	controllers/adCategories.php
    	controllers/vueCategories.php
    	model/vueCategories.php
    	model/vueSubCategories.php
    	template/layout.php

---
+++ controllers/subCategories.php
@@ -0,0 +1,34 @@
+<?php
+require_once('model/vueSubCategories.php');
+require_once('model/vueCategories.php');
+require_once('model/subCategories.php');
+
+
+function vueSubCategories($id)
+{
+    $subs = subCategories($id);
+    require('template/subCategories.php');
+}
+
+function vueAdSubCategories()
+{
+    $categories = categories();
+    require('template/adSubCategories.php');
+}
+
+function adSubCategories($nom, $id_categories)
+{
+    adSubCat($nom, $id_categories);
+    header('location:admin.php');
+}
+
+if (
+    (isset($_POST['sub_categories']) || !empty($_POST['sub_categories']))
+    || (isset($_POST['subCategoriesCategories']) || !empty($_POST['subCategoriesCategories']))
+
+) {
+    $nom = strip_tags($_POST['sub_categories']);
+
+    $id_categories = strip_tags($_POST['subCategoriesCategories']);
+    adSubCategories($nom, $id_categories);
+}
